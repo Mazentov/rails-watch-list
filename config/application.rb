573ec3eba46b85ff36b8e54cdd95ec4639c3aba5
Mazen  Dakdouk <mazendakdouk24@gmail.com>
2023-11-17 13:05:45 +0100

    problem asset precompile

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module RailsWatchList
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.assets.initialize_on_precompile = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
